bc7da7d0870976f16d347d02179d584b6ed14552
shaunk0111 <shaunk0111@gmail.com>
2018-05-02 10:36:16 +1000

    fixed char issues with SNMP response

--- src/main/java/chemtech/snmp/controllers/SnmpConnectionLinuxNative.java
+++ src/main/java/chemtech/snmp/controllers/SnmpConnectionLinuxNative.java
@@ -6,36 +6,39 @@ import chemtech.snmp.models.SnmpConfigModel;
 
 public class SnmpConnectionLinuxNative implements SnmpConnectionController {
 
-	SnmpConfigModel config;
-	
-	public SnmpConnectionLinuxNative(SnmpConfigModel config) {
-		this.config = config;
-	}
-	
-	@Override
-	public String getResponse(String oid) throws Exception {
-		
-		Process process;
-		BufferedReader reader;
+        SnmpConfigModel config;
 
-		String command = buildCommand(config.getCommunity(),config.getIP(),oid);
-		process = Runtime.getRuntime().exec(command);
-		process.waitFor();
-		reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        public SnmpConnectionLinuxNative(SnmpConfigModel config) {
+                this.config = config;
+        }
 
-		return reader.readLine();
-	}
-	
-	/**
-	 * snmpget Linux Native external command
-	 * @param community
-	 * @param host
-	 * @param oid
-	 * @return command String
-	 */
-	private String buildCommand(String community, String host, String oid) {
-		
-		return "snmpget -Oqv -v2c -c " + community + " " + host + " " + oid;
-	}
+        @Override
+        public String getResponse(String oid) throws Exception {
+
+                Process process;
+                BufferedReader reader;
+
+                String command = buildCommand(config.getCommunity(),config.getIP(),oid);
+                process = Runtime.getRuntime().exec(command);
+                process.waitFor();
+                reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String output = reader.readLine();
+                output = output.replaceAll("^\"|\"$", ""); // Remove char " from string
+                output = output.trim(); // Remove char empty 
+                return output;
+        }
+
+        /**
+         * snmpget Linux Native external command
+         * @param community
+         * @param host
+         * @param oid
+         * @return command String
+         */
+        private String buildCommand(String community, String host, String oid) {
+
+                return "snmpget -Oqv -v2c -c " + community + " " + host + " " + oid;
+        }
 
 }
+
